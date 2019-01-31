c79145a211e9df98653243e851e925dfdab0c44c
hb03-30 <hb03-30@BIT05-00>
2019-01-31 18:03:02 +0900

    파일저장 18:00 by ryu

--- SpringMVC/src/main/java/com/bitcamp/mvc/FileUploadController.java
+++ SpringMVC/src/main/java/com/bitcamp/mvc/FileUploadController.java
@@ -1,5 +1,8 @@
 package com.bitcamp.mvc;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -12,86 +15,77 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 @Controller
 public class FileUploadController {
 
-	
 	@RequestMapping("/fileupload/form")
 	public String form() {
 		return "fileupload/form";
 	}
-	
+
 	@RequestMapping("/fileupload/upload1")
 	public String getReport1(
-			
-			@RequestParam("sno") String sno,
-			@RequestParam("report") MultipartFile file,
-			Model model
-			
-			) {
-		
+
+			@RequestParam("sno") String sno, @RequestParam("report") MultipartFile file, Model model
+
+	) {
+
 		String fileName = file.getOriginalFilename();
 		Long fileSize = file.getSize();
-		
-		model.addAttribute("result"
-				, sno+"(첨부파일:"+fileName+ ": "+fileSize+")");
-		
+
+		model.addAttribute("result", sno + "(첨부파일:" + fileName + ": " + fileSize + ")");
+
 		return "fileupload/upload";
 	}
-	
+
 	@RequestMapping("/fileupload/upload2")
 	public String getReport2(
-			
-			MultipartHttpServletRequest request ,
-			Model model
-			
-			) {
-		
+
+			MultipartHttpServletRequest request, Model model
+
+	) {
+
 		String sno = request.getParameter("sno");
 		MultipartFile file = request.getFile("report");
-		
+
 		String fileName = file.getOriginalFilename();
 		Long fileSize = file.getSize();
-		
-		model.addAttribute("result"
-				, sno+"(첨부파일:"+fileName+ ": "+fileSize+")");
-		
+
+		model.addAttribute("result", sno + "(첨부파일:" + fileName + ": " + fileSize + ")");
+
 		return "fileupload/upload";
 	}
-	
 
-	
 	@RequestMapping("/fileupload/upload3")
 	public String getReport3(
-						
-			Report report ,
-			Model model
-			
-			) {
-		
+
+			HttpServletRequest request, Report report, Model model
+
+	) {
+
+		String uri = "/upload";
+		String dir = request.getSession().getServletContext().getRealPath(uri);
+
 		String sno = report.getSno();
 		MultipartFile file = report.getReport();
-		
+
 		String fileName = file.getOriginalFilename();
 		Long fileSize = file.getSize();
-		
-		model.addAttribute("result"
-				, sno+"(첨부파일:"+fileName+ ": "+fileSize+")");
-		
+
+		model.addAttribute("result", sno + "(첨부파일:" + fileName + ": " + fileSize + ")");
+
+		if (!file.isEmpty()) {
+			try {
+
+				file.transferTo(new File(dir, fileName));
+
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
 		return "fileupload/upload3";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
